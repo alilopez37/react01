f5dbe1dd59091f4984bb4b6328a91edaf2088607
alilopez <alzunun@upchiapas.edu.mx>
2020-12-21 19:58:14 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let mensaje = 'Hola Ali'
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hola Mundo</h1>
+        <h1> {mensaje}</h1>
       </header>
     </div>
   );
